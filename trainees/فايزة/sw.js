b3675aa9a56ccb8b7838bed347a2553a171cfb70
Faiz9-sys <abo3li2013@gmail.com>
2026-06-12 21:12:50 +0300

    sw فايزة

---
+++ trainees/فايزة/sw.js
@@ -0,0 +1,10 @@
+const C='jdwl-1781287964891';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./'])).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET'||!e.request.url.startsWith(self.location.origin))return;
+  e.respondWith(caches.match(e.request,{ignoreSearch:false}).then(hit=>{
+    const net=fetch(e.request).then(r=>{if(r.ok){const cl=r.clone();caches.open(C).then(c=>c.put(e.request,cl));}return r;}).catch(()=>hit||caches.match('./'));
+    return hit||net;
+  }));
+});
