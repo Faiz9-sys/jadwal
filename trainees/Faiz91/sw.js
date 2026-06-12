5aa16f9ca3ea5145e4b1f6bf262a29b3110cd915
Faiz9-sys <abo3li2013@gmail.com>
2026-06-12 21:11:39 +0300

    sw Faiz91

--- trainees/Faiz91/sw.js
+++ trainees/Faiz91/sw.js
@@ -1,4 +1,4 @@
-const C='jdwl-1781272326090';
+const C='jdwl-1781287893996';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./'])).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
