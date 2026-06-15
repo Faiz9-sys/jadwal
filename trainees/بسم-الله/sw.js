34437c548424e7e2cb890437966e78e40396dbc4
Faiz9-sys <abo3li2013@gmail.com>
2026-06-15 23:13:23 +0300

    sw بسم الله

--- trainees/بسم-الله/sw.js
+++ trainees/بسم-الله/sw.js
@@ -1,4 +1,4 @@
-const C='jdwl-1781554319869';
+const C='jdwl-1781554397710';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./'])).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
