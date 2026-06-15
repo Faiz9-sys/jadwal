63bbddae7953fa0a072f84363c4ae1a1d47f0069
Faiz9-sys <abo3li2013@gmail.com>
2026-06-15 18:36:46 +0300

    sw واحد اثنين

--- trainees/واحد-اثنين/sw.js
+++ trainees/واحد-اثنين/sw.js
@@ -1,4 +1,4 @@
-const C='jdwl-1781537049138';
+const C='jdwl-1781537800831';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./'])).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
