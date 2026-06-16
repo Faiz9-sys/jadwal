b963c2664590445813532ccaa4aa34bb999addb2
Faiz9-sys <abo3li2013@gmail.com>
2026-06-16 16:11:17 +0300

    sw خلود

--- trainees/خلود/sw.js
+++ trainees/خلود/sw.js
@@ -1,4 +1,4 @@
-const C='jdwl-1781582724369';
+const C='jdwl-1781615471676';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./'])).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
