3024003c28ef705d99c9381f015fbe8fa5215177
Faiz9-sys <abo3li2013@gmail.com>
2026-06-20 02:31:01 +0300

    sw رور الغامدي

--- trainees/رور-الغامدي/sw.js
+++ trainees/رور-الغامدي/sw.js
@@ -1,4 +1,4 @@
-const C='jdwl-1781911830111';
+const C='jdwl-1781911857437';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./'])).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
