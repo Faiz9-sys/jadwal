6e22be67f72dbbb707caf96a58621c37f89c0f11
Faiz9-sys <abo3li2013@gmail.com>
2026-06-23 10:30:32 +0300

    sw محمد المالكي

--- trainees/محمد-المالكي/sw.js
+++ trainees/محمد-المالكي/sw.js
@@ -1,4 +1,4 @@
-const C='jdwl-1782128248507';
+const C='jdwl-1782199827017';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./'])).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
