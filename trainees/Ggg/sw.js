09d7ba392b51d2b9c5f1fbc89edcaf3710fdde6d
Faiz9-sys <abo3li2013@gmail.com>
2026-06-24 23:01:06 +0300

    sw Ggg

--- trainees/Ggg/sw.js
+++ trainees/Ggg/sw.js
@@ -1,4 +1,4 @@
-const C='jdwl-1782331151562';
+const C='jdwl-1782331261079';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./'])).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
