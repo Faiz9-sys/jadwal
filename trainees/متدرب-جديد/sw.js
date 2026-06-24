75d7af356dc22306bb1dd55ab8b343c462b86cb5
Faiz9-sys <abo3li2013@gmail.com>
2026-06-24 17:11:30 +0300

    sw متدرب جديد

--- trainees/متدرب-جديد/sw.js
+++ trainees/متدرب-جديد/sw.js
@@ -1,4 +1,4 @@
-const C='jdwl-1782310008845';
+const C='jdwl-1782310284686';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./'])).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{
